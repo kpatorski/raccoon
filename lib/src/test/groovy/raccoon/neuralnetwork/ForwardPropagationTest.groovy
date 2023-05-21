3394c2e407df6346a2407bbbdd923c8a01796a02
kpatorski <patorski.krzysztof@gmail.com>
2023-05-21 14:14:31 +0200

    Add test for neural network without hidden layers

--- lib/src/test/groovy/raccoon/neuralnetwork/ForwardPropagationTest.groovy
+++ lib/src/test/groovy/raccoon/neuralnetwork/ForwardPropagationTest.groovy
@@ -7,7 +7,26 @@ import static raccoon.neuralnetwork.Link.Weight
 
 class ForwardPropagationTest extends Specification {
 
-    def "input neurons transmit signals to output neuron"() {
+    def "input neurons transmit signals to the outputs "() {
+        given:
+        def weights = fixedWeights([0.5d, 1, 0.5d, 1])
+
+        and:
+        NeuralNetwork network = BuildNetwork.ofFixedWeights(weights)
+                .inputLayer(2)
+                .outputLayer(2, linearFunction())
+
+        expect:
+        network.inputs([signal(input1), signal(input2)]).outputs() == [signal(outputs[0]), signal(outputs[1])]
+
+        where:
+        input1 | input2 || outputs
+        0      | 0      || [0d, 0d]
+        0.5d   | 0.5d   || [0.5d, 1d]
+        1      | 1      || [1d, 2d]
+    }
+
+    def "input neurons transmit signals to output neuron through hidden neurons"() {
         given:
         def weights = fixedWeights([0.1d, 0.1d, 0.1d, 0.2d, 0.2d, 0.2d, 0.3d, 0.3d, 0.3d, 0.3d, 0.3d, 0.3d, 0.4d, 0.4d, 0.5d, 0.5d])
 
